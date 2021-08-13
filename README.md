354d188e9a1aaea3d3c564e275e8808f29343405
jeff-snyk <75397394+jeff-snyk@users.noreply.github.com>
2021-08-12 19:51:14 -0700

    Features (#2)
    * Made arguments named
    
    * added argument for version
    
    * Added ability to use snyk-to-html
    
    * updated readme and --help message

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # snyk-scan.sh
 prototype monorepo utility wrapper for Snyk CLI
-### Examples
+###  Basic Examples
 ```
-snyk-scan.sh test java_gradle
-```
-```
-snyk-scan.sh test all
+snyk-scan.sh --mode=test --type=java_gradle
 ```
 ```
-snyk-scan.sh monitor javascript
+snyk-scan.sh --mode=test --type=all
 ```
 ```
-snyk-scan.sh monitor all
+snyk-scan.sh --mode=monitor --type=javascript
+```
+```
+snyk-scan.sh --mode=monitor --type=all
+```
+### Other uses
+You can also provide a version string, if you want to track multiple versions of the same repo/app
+```
+snyk-scan.sh --mode=monitor --version=1.2.3
+```
+You can also generate a local html report for each project tested. This will result in a test only, regardless of --mode value.  If you require monitor, call the script twice.
+This requires [https://github.com/snyk/snyk-to-html](https://github.com/snyk/snyk-to-html) to be available.
+```
+snyk-scan.sh --html=1 --type=all
 ```
--- snyk-scan.sh
+++ snyk-scan.sh
@@ -5,14 +5,79 @@
 # recurse through directory structure and snyk scan each 
 # project for a given list of file types
 #
-# $1 -> scanMode: one of [monitor, test]
-# $2 -> projectType: one of [
+# --mode= -> scanMode: one of [monitor, test]
+# --type= -> projectType: one of [
 #   javascript, python, java_maven, java_gradle, 
 #   dotnet, ruby, golang, cocoapods, scala, php, all
 # ]
 # all is a special case that will iterate through each type
-scanMode=$1
-projectType=$2
+#
+# --version= -> versionString: this can be used to enable tracking multiple
+#   versions of an app.  This will be appended to all project names
+#   and the project group.
+# --args= -> extraArgs: this is snyk CLI arguments which will be applied on 
+#   every test/monitor command.  Don't use --project-name or --remote-repo-url
+#   or --all-projects or --exclude
+#
+# --html= -> htmlReport: this indicates if snyk-to-html should be used to 
+#   generate local html reports.  Only works with 'test'.
+#   Any --args will be discareded.
+#   Snyk-to-html must be installed: https://github.com/snyk/snyk-to-html
+
+htmlReport=0
+rootDir="$(pwd)"
+
+# logic taken from https://unix.stackexchange.com/a/580258
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --mode*)
+      if [[ "$1" != *=* ]]; then shift; fi # Value is next arg if no `=`
+      scanMode="${1#*=}"
+      ;;
+    --type*)
+      if [[ "$1" != *=* ]]; then shift; fi
+      projectType="${1#*=}"
+      ;;
+    --version*)
+      if [[ "$1" != *=* ]]; then shift; fi
+      versionString="${1#*=}"
+      ;;
+    --args*)
+      if [[ "$1" != *=* ]]; then shift; fi
+      extraArgs="${1#*=}"
+      ;;
+    --html*)
+      if [[ "$1" != *=* ]]; then shift; fi
+      htmlReport="${1#*=}"
+      ;;
+    --help|-h)
+      echo ""
+      echo "This is a prototype script to help use the Snyk CLI for monorepos."
+      echo ""
+      echo "--mode allows you to choose between test or monitor."
+      echo "--type allows you to choose the language/package manager to look for:"
+      echo "    snyk-scan.sh --mode=test --type=java_gradle"
+      echo "    snyk-scan.sh --mode=monitor --type=all"
+      echo ""
+      echo "--version allows you to specify a version string if you want to track"
+      echo "  multiple versions of the same monorepo:"
+      echo "    snyk-scan.sh --mode=test --type=java_gradle --version=1.2.3"
+      echo ""
+      echo "--html will do a test and provide local html reports."
+      echo "  You must already have https://github.com/snyk/snyk-to-html available:"
+      echo "    snyk-scan.sh --html=1 --type=all"
+      echo ""
+      exit 0
+      ;;
+    *)
+      >&2 echo "Error: Invalid argument"
+      >&2 echo ""
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 
 # set default exitCode for the entire set of scans
 finalExitCode=0
@@ -34,17 +99,32 @@ projectTypes_php="composer.lock"
 
 echo "scanMode set to: ${scanMode}"
 echo "projectType set to: ${projectType}"
+echo "using extraArgs: ${extraArgs}"
 
 # snyk monitor snapshots will be stored under this grouping
-projectGroup=$(basename `pwd`)
+if [[ -n "$versionString" ]]; then
+    projectGroup="$(basename `pwd`)-${versionString}"
+else
+    projectGroup=$(basename `pwd`)
+fi
 
 echo "projectGroup set to: ${projectGroup}"
 
 snyk_scan(){
     echo "testing manifest: ${1}"
-    projectName="${1:2}"
+    if [[ -n "$versionString" ]]; then
+        projectName="${1:2}-${versionString}"
+    else
+        projectName="${1:2}"
+    fi
     echo "project name: ${projectName}"
-    snyk $scanMode --file="${1}" --project-name="${projectName}" --remote-repo-url="${projectGroup}"
+    if [[ "$htmlReport" == "1" ]]; then
+        file_name="${rootDir}/$(echo $projectName | tr '/' '_').html"
+        echo "${file_name}"
+        snyk test --file="${1}" --json | snyk-to-html -o "${file_name}"
+    else
+        snyk $scanMode --file="${1}" --project-name="${projectName}" --remote-repo-url="${projectGroup}" "$extraArgs"
+    fi
     return $?
 }
 
@@ -94,8 +174,10 @@ else # iterate through all known project types
 fi
 
 echo ""
-
-if [[ "${scanMode}" == "test" ]]; then
+if [[ "$htmlReport" == "1" ]]; then
+    echo "Completed generating reports"
+    echo " - (2) Error: ${numExitCodes[2]}"
+elif [[ "${scanMode}" == "test" ]]; then
     echo "Project Test Summary by Exit Code:"
     echo " - (0) No Vulns: ${numExitCodes[0]}"
     echo " - (1) Vulns: ${numExitCodes[1]}"
